92190e21f88c954f48915c68ac27517e5d6d43ca
Jamie Weldon <jamie.weldon@outlook.com>
2024-02-06 08:10:39 -0600

    initial commit

---
+++ .compiled/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _helloWorld = require("./helloWorld.mjs");
+
+new _helloWorld.HelloWorld().sayHello();
+//# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import {HelloWorld} from "./helloWorld.mjs";
+new HelloWorld().sayHello();
